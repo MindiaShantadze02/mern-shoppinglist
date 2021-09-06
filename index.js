61f9647a1eaa0fc1cefb7df5f0fe9834965ea413
Mindia Shantadze <shantadze200202@gmail.com>
2021-09-06 18:04:16 +0400

    Another commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
-const PORT = process.env.PORT || 5000;
 // local imports
 const router = require("./routes/item");
 const connectDB = require("./config/db");
@@ -19,14 +18,9 @@ app.use(express.json());
 // using router
 app.use("/api",router);
 
-app.use(express.static(path.join(__dirname, '../build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'));
-})
-
 // connecting to database
 connectDB();
 
 
 // listening to the server
-app.listen(process.env.PORT, ()=> console.log(`Server is running on port: ${PORT}`));
+app.listen(process.env.PORT, ()=> console.log(`Server is running on port: ${process.env.PORT}`));
